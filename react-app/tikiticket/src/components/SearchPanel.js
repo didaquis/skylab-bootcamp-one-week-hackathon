8fed6ee9ef09971007c4a952e3ea19c9af91e079
Dídac García <didaquis@gmail.com>
2018-02-12 17:46:02 +0100

    :pencil: Los selectores adquieren valores lógicos

--- react-app/tikiticket/src/components/SearchPanel.js
+++ react-app/tikiticket/src/components/SearchPanel.js
@@ -5,11 +5,15 @@ import InputButton from './InputButton';
 import GoButton from './GoButton';
 
 function SearchPanel(props){
+
+    const optionsForSelectOne = [{"identifier": "bcn", "text":"Barcelona"}, {"identifier": "mad", "text":"Madrid"}];
+    const optionsForSelectTwo = [{"identifier": "ms", "text":"Music"}, {"identifier": "sp", "text":"Sports"}, {"identifier": "at", "text":"Art & theater"}, {"identifier": "ot", "text":"Others"}];
+
 	return (
 		<section className="container-fluid searchbar">
             <div className="row">
-            	<SelectButton textButton={'City'}/>
-            	<SelectButton textButton={'What?'}/>
+            	<SelectButton textButton={'City'} dataForOptions={optionsForSelectOne} />
+            	<SelectButton textButton={'What?'} dataForOptions={optionsForSelectTwo} />
             	<InputButton />
             	<GoButton />
             </div>
