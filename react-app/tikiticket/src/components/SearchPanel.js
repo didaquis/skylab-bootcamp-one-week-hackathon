3fa64639ee3757dfc167edebf89f912d368ebfed
Eahob <lerbfae@gmail.com>
2018-02-13 12:36:05 +0100

    updatet component searchpanel.js

--- react-app/tikiticket/src/components/SearchPanel.js
+++ react-app/tikiticket/src/components/SearchPanel.js
@@ -4,20 +4,24 @@ import SelectButton from './SelectButton';
 import InputButton from './InputButton';
 import GoButton from './GoButton';
 
-function SearchPanel(props){
+function SearchPanel(props) {
 
-    const contentForSelect_One = [{"identifier": "bcn", "text":"Barcelona"}, {"identifier": "mad", "text":"Madrid"}];
-    const contentForSelect_Two = [{"identifier": "ms", "text":"Music"}, {"identifier": "sp", "text":"Sports"}, {"identifier": "at", "text":"Art & theater"}, {"identifier": "ot", "text":"Others"}];
+	const contentForSelect_One = [{ "identifier": "bcn", "text": "Barcelona" }, { "identifier": "mad", "text": "Madrid" }];
+	const contentForSelect_Two = [{ "identifier": "ms", "text": "Music" }, { "identifier": "sp", "text": "Sports" }, { "identifier": "at", "text": "Art & theater" }, { "identifier": "ot", "text": "Others" }];
 
 	return (
 		<section className="container-fluid searchbar">
-            <div className="row">
-            	<SelectButton textButton={'City'} dataForComponent={contentForSelect_One} />
-            	<SelectButton textButton={'What?'} dataForComponent={contentForSelect_Two} />
-            	<InputButton />
-            	<GoButton />
-            </div>
-        </section>
+			<form>
+				<div class="form-group">
+					<div className="row">
+						<SelectButton textButton={'City'} dataForComponent={contentForSelect_One} />
+						<SelectButton textButton={'What?'} dataForComponent={contentForSelect_Two} />
+						<InputButton />
+						<GoButton />
+					</div>
+				</div>
+			</form>
+		</section>
 	);
 }
 
