f5dbd8e3a5a3d891bfd23751ea42daf713c0d911
Dídac García <didaquis@gmail.com>
2018-02-13 13:53:07 +0100

    :pencil: EventList ya pasar props a EventBox

--- react-app/tikiticket/src/components/EventList.js
+++ react-app/tikiticket/src/components/EventList.js
@@ -4,11 +4,29 @@ import EventBox from './EventBox';
 
 function EventList(props){
 	const exampleOfEventIdentifier = 'Z598xZ2qZ6Akk';/* Useful just for development logic */
+	const exampleOfTitle = ' Jessy & Natalia Lafourcade';/* Useful just for development logic */
+	const exampleOfTypeOfEvent = 'Music';/* Useful just for development logic */
+	const exampleOfhashtags = ['Alternative Rock', 'Music', 'Pop'];/* Useful just for development logic */
+	const exampleOfDate = '2018-11-07';/* Useful just for development logic */
 	return (
 		<div className="col-sm-6 col-md-4">
 			<ul className="list-group">
-				<EventBox eventId={exampleOfEventIdentifier} onClick={props.onClickEvent} />
-				<EventBox eventId={exampleOfEventIdentifier} onClick={props.onClickEvent} />
+				<EventBox 
+					eventId={exampleOfEventIdentifier} 
+					eventTitle={exampleOfTitle} 
+					eventType={exampleOfTypeOfEvent} 
+					eventHashtags={exampleOfhashtags} 
+					eventDate={exampleOfDate} 
+					onClick={props.onClickEvent} 
+				/>
+				<EventBox 
+					eventId={exampleOfEventIdentifier} 
+					eventTitle={exampleOfTitle} 
+					eventType={exampleOfTypeOfEvent} 
+					eventHashtags={exampleOfhashtags} 
+					eventDate={exampleOfDate} 
+					onClick={props.onClickEvent} 
+				/>
 			</ul>
 		</div>
 	);
