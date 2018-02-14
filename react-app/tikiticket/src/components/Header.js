e66c48dfeaa7c1180dc6338ed7e4bc1e4234224c
Eahob <lerbfae@gmail.com>
2018-02-14 18:51:26 +0100

    modified CSS and Header.js according to new design

--- react-app/tikiticket/src/components/Header.js
+++ react-app/tikiticket/src/components/Header.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 function Header(props) {
 	return (
-		<header className="container-fluid d-none d-sm-block">
-			<div className="row">
-				<div className="d-none d-sm-block col-lg-9">
+		<header className="container-fluid">
+			<div className="row ">
+				<div className="d-none d-sm-block col-lg-3 tiki" />
+				<div className="col-lg-6 title">
 					<h1>Tiki Ticket</h1>
 				</div>
 				<div className="d-none d-lg-block col-lg-3">
 					<h3>The ancient
-					<br />event finder
+						<br />event finder
 					</h3>
 				</div>
 			</div>
